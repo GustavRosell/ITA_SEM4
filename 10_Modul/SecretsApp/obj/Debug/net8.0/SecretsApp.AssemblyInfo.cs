f0fa6cbcca5f6d27826105a2e9963eda56a0272a
rosell <Gustavrosell@gmail.com>
2024-11-05 13:13:34 +0100

    ?

--- 10_Modul/SecretsApp/obj/Debug/net8.0/SecretsApp.AssemblyInfo.cs
+++ 10_Modul/SecretsApp/obj/Debug/net8.0/SecretsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecretsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df02af541ede77d75099dce8644bd325aeb523d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bbf6ca98dcf45ffb0ad4e7bc9bc3225d583b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecretsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecretsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 7_Modul/MessageService/obj/Debug/net8.0/MessageService.AssemblyInfo.cs
+++ 7_Modul/MessageService/obj/Debug/net8.0/MessageService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessageService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df02af541ede77d75099dce8644bd325aeb523d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bbf6ca98dcf45ffb0ad4e7bc9bc3225d583b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessageService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessageService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
